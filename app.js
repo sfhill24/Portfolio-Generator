0fdbbcb2226e636efdb0b455d9b4fd6d3955ffcd
Shelly Foxworth-Hill <sfhill24@gmail.com>
2022-09-20 16:43:10 -0400

    added features

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const profileDataArgs = process.argv.slice(2);
+/* const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
   // This...
@@ -13,3 +13,23 @@ const printProfileData = profileDataArr => {
 };
 
 printProfileData(profileDataArgs);
+ */
+
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
